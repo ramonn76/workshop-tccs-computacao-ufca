53f2edad6817650474ac409d13df9f2d6706d94b
Ramon Nepomuceno <ramonn76@gmail.com>
2023-06-06 11:14:41 -0300

    updating to 2022.2

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@
     
 
     // Use this for real timer date
-      var timerdate = "2022/12/16"; 
+      var timerdate = "2023/06/12"; 
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
